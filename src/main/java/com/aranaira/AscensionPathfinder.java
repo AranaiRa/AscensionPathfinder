6eb760ad638b43a57f07606b0118b667c7599457
AranaiRa <aranaiseraph@gmail.com>
2021-08-19 21:14:57 -0400

    Initial code commit

---
+++ src/main/java/com/aranaira/AscensionPathfinder.java
@@ -0,0 +1,21 @@
+package com.aranaira.handyshapes;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("ascpath")
+public class AscensionPathfinder {
+  public static final Logger LOG = LogManager.getLogger();
+  public static final String MODID = "ascpath";
+  public static final String VERSION = "GRADLE:VERSION";
+
+  public static ItemGroup itemGroup;
+
+  public AscensionPathfinder() {
+    IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+  }
+}
---
+++ src/main/java/com/aranaira/common/lib/LibItemNames.java
@@ -0,0 +1,5 @@
+package com.aranaira.handyshapes.common.lib;
+
+public class LibItemNames {
+    public static final String ACCESSIBILITY_WAND = "accessibility_wand";
+}
